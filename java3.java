3af93f3243986a4760c820a1885698251c6aed2c
MarinZaicev <Kirskripnikov@gmail.com>
2025-04-25 19:42:45 +0700

    Update java3.java

--- java3.java
+++ java3.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     
-    public static int pozition(int N) {
+    static int pozition(int N) {
         int pozition = -1;
         int number;
         int pozitionCount = 0;
@@ -23,19 +23,28 @@ public class Main {
             return 0;
         }
     }
-
+    
+    static void bags(int tryNumber) {
+        if (tryNumber < 0) {
+            System.out.println("вводите только положительное число");
+            System.exit(0);
+        }
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("сколько чисел вы планируете вводить?");
         int numberCount = scanner.nextInt();
+        bags(numberCount);
         
-        System.out.println("вводите числа");
+        System.out.println("вводите числа ");
         int number;
         int sum = 0;
         
         for (int i = numberCount; i > 0; i--) {
             number = scanner.nextInt();
+            bags(number);
             sum += pozition(number);
         }
         
