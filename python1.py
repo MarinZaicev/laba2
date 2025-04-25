5536f7e73d9ca3c5930fb68c763e15eec33c98ad
MarinZaicev <Kirskripnikov@gmail.com>
2025-04-25 19:17:11 +0700

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,45 +1,48 @@
 def flowers(water_count, volume):
-    x = volume  # объем лейки
-    steps = 0   # кол-во шагов
+    x = volume
+    steps = 0
     
     for i in range(len(water_count)):
-        if x - water_count[i] >= 0:  # если воды достаточно
+        if x - water_count[i] >= 0:
             x -= water_count[i]
             steps += 1
-        else:  # если воды недостаточно
-            x = volume  # набираем лейку у реки
-            steps += 2 * (i + 1) - 1  # шаги до реки и обратно
+        else:
+            x = volume
+            steps += 2 * (i + 1) - 1
             x -= water_count[i]
-    
     return steps
 
-def bags(flower_count):  # проверка входящих значений
+def bags(flower_count):
     if flower_count < 0:
         print("введено невозможное значение")
-        exit(0)  # завершаем программу при ошибке
+        exit(0)
 
-def main():
-    try:
-        flower_count = int(input("Введите количество цветов: "))
-        bags(flower_count)
-        
-        water_counts = []
-        print("Введите кол-во воды, нужное для каждого цветка:")
-        
-        for i in range(flower_count):
-            water = int(input(f"{i+1} цветок: "))
-            bags(water)
-            water_counts.append(water)
-        
-        volume = int(input("Введите объем лейки: "))
-        bags(volume)
-        
-        steps = flowers(water_counts, volume)
-        print(f"{steps} шагов вы сделаете")
-        
-    except ValueError:
+# Основной код
+user_input = input("Введите количество цветов: ")
+if not user_input.isdigit():  # Проверяем, что введено число
+    print("Ошибка: введите целое число!")
+    exit(1)
+flower_count = int(user_input)
+bags(flower_count)
+
+water_counts = []
+print("Введите кол-во воды, нужное для каждого цветка:")
+
+for i in range(flower_count):
+    user_input = input(f"{i+1} цветок: ")
+    if not user_input.isdigit():
         print("Ошибка: введите целое число!")
         exit(1)
+    water = int(user_input)
+    bags(water)
+    water_counts.append(water)
 
-if __name__ == "__main__":
-    main()
+user_input = input("Введите объем лейки: ")
+if not user_input.isdigit():
+    print("Ошибка: введите целое число!")
+    exit(1)
+volume = int(user_input)
+bags(volume)
+
+steps = flowers(water_counts, volume)
+print(f"{steps} шагов вы сделаете")
